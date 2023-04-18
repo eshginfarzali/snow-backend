6bf5ae6510d397ed15e183cd31daed9a2673ae8e
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-18 22:29:15 +0400

    feat: schema

---
+++ server/data/index.js
@@ -0,0 +1,73 @@
+
+
+export const dataUser =[
+    {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "password",
+      role: "user",
+     
+    },
+    {
+      name: "Jane Doe",
+      email: "janedoe@example.com",
+      password: "password",
+      role: "user",
+      
+    },
+    {
+      name: "Bob Smith",
+      email: "bobsmith@example.com",
+      password: "password",
+      role: "user",
+     
+    },
+    {
+      name: "Alice Smith",
+      email: "alicesmith@example.com",
+      password: "password",
+      role: "user",
+     
+    },
+    {
+      name: "Mike Johnson",
+      email: "mikejohnson@example.com",
+      password: "password",
+      role: "user",
+     
+    },
+    {
+      name:"Samantha Johnson", 
+      email:"samanthajohnson@example.com", 
+      password:"password", 
+      role:"user", 
+      
+    },
+    {
+        name:"David Lee", 
+        email:"davidlee@example.com", 
+        password:"password", 
+        role:"user", 
+       
+    },
+    {
+        name:"Emily Lee", 
+        email:"emilylee@example.com", 
+        password:"password", 
+        role:"user", 
+        
+    },
+    {
+        name:"Tommy Nguyen", 
+        email:"tommynguyen@example.com", 
+        password:"password", 
+        role:"user", 
+       
+    },
+    {
+        name:"Linda Nguyen", 
+        email:"lindanguyen@example.com", 
+        password:"password", 
+        role:"user", 
+    }
+  ]
---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from "dotenv";
+import categoryRoutes from "./routes/category.js"
+import expeditorsRoutes from "./routes/expeditors.js"
+import managementRoutes from "./routes/management.js"
+import productsRoutes from "./routes/products.js"
+import salesRoutes from "./routes/sales.js"
+import subcategoryRoutes from "./routes/subcategory.js"
+
+//data imports
+import User from "./models/user.js"
+import { dataUser } from './data/index.js';
+
+
+/* CONFIGURATION*/
+
+dotenv.config();
+const app =express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+/* ROUTES */
+app.use("category", categoryRoutes);
+app.use("/expeditors", expeditorsRoutes);
+app.use("/management", managementRoutes);
+app.use("/products", productsRoutes);
+app.use("/sales", salesRoutes);
+app.use("/subcategory", subcategoryRoutes);
+
+
+
+/*MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(()=>{
+        app.listen(PORT, ()=> console.log(`Server Port : ${PORT}`));
+            /* ONLY ADD DATA ONE TIME */
+        User.insertMany(dataUser)
+    })
